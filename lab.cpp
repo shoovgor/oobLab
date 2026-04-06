ed1f037ec96122f9d127db2e10b7e4c4e354f7ed
shoovgor <purevjav.dgl@gmail.com>
2026-04-06 22:23:44 +0800

    Point, Shape, Shape2D bolon tur main nemev

--- lab.cpp
+++ lab.cpp
@@ -1,1 +1,41 @@
-//test
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+// Координат хадгалах бүтэц
+struct Point {
+    double x, y;
+};
+
+// Дүрсийн ерөнхий класс
+class Shape {
+protected:
+    string name;
+
+public:
+    Shape(const string& n = "") : name(n) {}   // байгуулагч функц
+    virtual ~Shape() {}   // virtual устгагч, хүүхэд классууд зөв устахад хэрэгтэй
+};
+
+// 2 хэмжээст дүрсийн ерөнхий хийсвэр класс
+class Shape2D : public Shape {
+protected:
+    Point refPoint;   // дүрсийн ерөнхий нэг цэг хадгална
+
+public:
+    Shape2D(const string& n, Point p) : Shape(n), refPoint(p) {}   // байгуулагч функц
+
+    // Эдгээр функцыг зөвхөн хүүхэд классууд хэрэгжүүлнэ
+    virtual double area() const = 0;
+    virtual double perimeter() const = 0;
+    virtual void printInfo() const = 0;
+};
+
+// PI тогтмол
+const double PI = acos(-1.0);
+
+int main() {
+    cout << "Suuri classuud amjilttai uuslee." << endl;
+    return 0;
+}
