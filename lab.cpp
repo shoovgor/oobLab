dab9511563b7595d63a6ee8f5c412983e94fd3d2
shoovgor <purevjav.dgl@gmail.com>
2026-04-07 09:11:34 +0800

    finalUpdate

--- lab.cpp
+++ lab.cpp
@@ -2,57 +2,52 @@
 #include <cmath>
 #include <string>
 using namespace std;
- 
+
 // Координат хадгалах бүтэц
 struct Point {
     double x, y;
 };
- 
+
 // Дүрсийн ерөнхий класс
 class Shape {
 protected:
     string name;
- 
+
 public:
-    Shape(const string& n = "") : name(n) {}   // байгуулагч функц
-    virtual ~Shape() {}   // virtual устгагч, хүүхэд классууд зөв устахад хэрэгтэй
+    Shape(const string& n = "") : name(n) {}
+    ~Shape() {}
 };
- 
-// 2 хэмжээст дүрсийн ерөнхий хийсвэр класс
+
+// 2 хэмжээст дүрсийн ерөнхий класс
 class Shape2D : public Shape {
 protected:
-    Point refPoint;   // дүрсийн ерөнхий нэг цэг хадгална
- 
+    Point refPoint;
+
 public:
-    Shape2D(const string& n, Point p) : Shape(n), refPoint(p) {}   // байгуулагч функц
- 
-    // Эдгээр функцыг зөвхөн хүүхэд классууд хэрэгжүүлнэ
-    virtual double area() const = 0;
-    virtual double perimeter() const = 0;
-    virtual void printInfo() const = 0;
+    Shape2D(const string& n, Point p) : Shape(n), refPoint(p) {}
 };
- 
+
 // PI тогтмол
 const double PI = acos(-1.0);
- 
+
 // Тойргийн класс
 class Circle : public Shape2D {
 private:
     double radius;
- 
+
 public:
     Circle(const string& n, Point center, double r)
-        : Shape2D(n, center), radius(r) {}   // n-нэр, center-төв цэг, r-радиус
- 
-    double area() const override {
+            : Shape2D(n, center), radius(r) {}
+
+    double area() const {
         return PI * radius * radius;
     }
- 
-    double perimeter() const override {
+
+    double perimeter() const {
         return 2 * PI * radius;
     }
- 
-    void printInfo() const override {
+
+    void printInfo() const {
         cout << "===== Circle =====\n";
         cout << "Ner: " << name << "\n";
         cout << "Tuv: (" << refPoint.x << ", " << refPoint.y << ")\n";
@@ -61,35 +56,35 @@ public:
         cout << "Hureenii urt: " << perimeter() << "\n\n";
     }
 };
- 
+
 // Квадратын класс
 class Square : public Shape2D {
 private:
     double side;
     Point A, B, C, D;
- 
-    void calculateVertices() {   // оройнуудыг тооцоолох туслах функц
+
+    void calculateVertices() {
         A = refPoint;
         B = {refPoint.x + side, refPoint.y};
         C = {refPoint.x + side, refPoint.y - side};
         D = {refPoint.x, refPoint.y - side};
     }
- 
+
 public:
     Square(const string& n, Point topLeft, double s)
-        : Shape2D(n, topLeft), side(s) {
+            : Shape2D(n, topLeft), side(s) {
         calculateVertices();
     }
- 
-    double area() const override {
+
+    double area() const {
         return side * side;
     }
- 
-    double perimeter() const override {
+
+    double perimeter() const {
         return 4 * side;
     }
- 
-    void printInfo() const override {
+
+    void printInfo() const {
         cout << "===== Square =====\n";
         cout << "Ner: " << name << "\n";
         cout << "Taliin urt: " << side << "\n";
@@ -102,15 +97,16 @@ public:
         cout << "Hureenii urt: " << perimeter() << "\n\n";
     }
 };
- // Гурвалжин класс
+
+// Гурвалжин класс
 class Triangle : public Shape2D {
 private:
     double side;
     Point A, B, C;
 
-    void calculateVertices() { // tuslah function
+    void calculateVertices() {
         double h = sqrt(3.0) / 2.0 * side;
-        A = refPoint; // oroin tseg
+        A = refPoint;
         B = {refPoint.x - side / 2.0, refPoint.y - h};
         C = {refPoint.x + side / 2.0, refPoint.y - h};
     }
@@ -121,15 +117,15 @@ public:
         calculateVertices();
     }
 
-    double area() const override {
+    double area() const {
         return sqrt(3.0) / 4.0 * side * side;
     }
 
-    double perimeter() const override {
+    double perimeter() const {
         return 3 * side;
     }
 
-    void printInfo() const override {
+    void printInfo() const {
         cout << "===== Zuv Gurvaljin =====\n";
         cout << "Ner: " << name << "\n";
         cout << "Taliin urt: " << side << "\n";
@@ -149,15 +145,9 @@ int main() {
     Square s("S1", {0, 10}, 4);
     Triangle t("T1", {0, 8}, 6);
 
-    Shape2D* shapes[3];
-    shapes[0] = &c;
-    shapes[1] = &s;
-    shapes[2] = &t;
-
-    // buh dursiin medeelliig hevleh
-    for (int i = 0; i < 3; i++) {
-        shapes[i]->printInfo();
-    }
+    c.printInfo();
+    s.printInfo();
+    t.printInfo();
 
     return 0;
 }
