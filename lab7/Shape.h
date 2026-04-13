16dc2450e3e2c8156ebd178b4549f81eb7305a62
Person B <24b1num2408@stud.num.edu.mn>
2026-04-13 22:26:05 +0800

    Shape.h deer getter nemsen

--- lab7/Shape.h
+++ lab7/Shape.h
@@ -2,16 +2,21 @@
 #define SHAPE_H
 
 #include <string>
-using namespace std; 
+using namespace std;
 
 class Shape {
 protected:
     string name;
 
 public:
-    Shape(const string& n = ""){
+    Shape(const string& n = "") {
         name = n;
     }
+
+    string getName() const {
+        return name;
+    }
+
     virtual ~Shape() {}
 };
 
