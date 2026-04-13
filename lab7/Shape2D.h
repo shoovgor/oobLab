77129ff3bfe47bc1bc7b69f1641d7755ea91b973
Person B <24b1num2408@stud.num.edu.mn>
2026-04-13 22:09:05 +0800

    2d classiig  abstract class bolgoson

--- lab7/Shape2D.h
+++ lab7/Shape2D.h
@@ -1,5 +1,6 @@
 #ifndef SHAPE2D_H
-#define SHAPE2D_H  
+#define SHAPE2D_H
+
 #include "Shape.h"
 #include "Point.h"
 
@@ -11,5 +12,11 @@ public:
     Shape2D(const string& n, Point p) : Shape(n) {
         refPoint = p;
     }
+
+    virtual double area() const = 0;
+    virtual double perimeter() const = 0;
+    virtual void printInfo() const = 0;
+    virtual ~Shape2D() {}
 };
+
 #endif
