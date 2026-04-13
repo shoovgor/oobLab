1711046d9727e96ea1a93a545a934e130c0a34aa
Person B <24b1num2408@stud.num.edu.mn>
2026-04-13 22:14:15 +0800

    olon object uusgej talbaigar erembelsen

--- lab7/lab.cpp
+++ lab7/lab.cpp
@@ -1,33 +1,46 @@
-    #include <iostream>
-    #include <cmath>
-    #include <string>
-    #include "Shape.h"
-    #include "Point.h"
-    #include "Shape2D.h"
-    #include "Circle.h"
-    #include "Square.h"
-    #include "Triangle.h"
-    #include "PI.h"
-    using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include "Point.h"
+#include "Shape.h"
+#include "Shape2D.h"
+#include "Circle.h"
+#include "Square.h"
+#include "Triangle.h"
+#include "PI.h"
+
+using namespace std;
 
 int main() {
-    cout << "\n";
-    Circle c1("C1", {2, 3}, 5);
-    Circle c2("C2", {0, 0}, 2);
+    vector<Shape2D*> shapes;
 
-    Square s1("S1", {0, 10}, 4);
-    Square s2("S2", {5, 5}, 3);
+    shapes.push_back(new Circle("C1", {2, 3}, 5));
+    shapes.push_back(new Circle("C2", {0, 0}, 2));
 
-    Triangle t1("T1", {0, 8}, 6);
-    Triangle t2("T2", {2, 7}, 4);
+    shapes.push_back(new Square("S1", {0, 10}, 4));
+    shapes.push_back(new Square("S2", {5, 5}, 3));
 
-    c1.printInfo();
-    c2.printInfo();
+    shapes.push_back(new Triangle("T1", {0, 8}, 6));
+    shapes.push_back(new Triangle("T2", {2, 7}, 4));
 
-    s1.printInfo();
-    s2.printInfo();
+    cout << "=== Anhnii baidal ===\n\n";
+    for (Shape2D* s : shapes) {
+        s->printInfo();
+    }
 
-    t1.printInfo();
-    t2.printInfo();
-return 0;
+    sort(shapes.begin(), shapes.end(), [](Shape2D* a, Shape2D* b) {
+        return a->area() < b->area();
+    });
+
+    cout << "=== Talbaigaar erembelsen ===\n\n";
+    for (Shape2D* s : shapes) {
+        s->printInfo();
+    }
+
+    for (Shape2D* s : shapes) {
+        delete s;
+    }
+
+    return 0;
 }
