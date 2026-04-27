c3edf23f7ff13f7b8dced01a84f6bce285570d06
Person B <24b1num2408@stud.num.edu.mn>
2026-04-27 22:44:13 +0800

    Triange classig this ashigladag bolgoson

--- lab7/Triangle.h
+++ lab7/Triangle.h
@@ -1,47 +1,49 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
-
+ 
 #include "Shape2D.h"
 #include "Point.h"
 #include <iostream>
+#include <cmath>
 using namespace std;
-
+ 
 class Triangle : public Shape2D {
 private:
     double side;
     Point A, B, C;
-
+ 
     void calculateVertices() {
-        double h = sqrt(3.0) / 2.0 * side;
-        A = refPoint;
-        B = {refPoint.x - side / 2.0, refPoint.y - h};
-        C = {refPoint.x + side / 2.0, refPoint.y - h};
+        double h = sqrt(3.0) / 2.0 * this->side;
+        this->A = this->refPoint;
+        this->B = {this->refPoint.x - this->side / 2.0, this->refPoint.y - h};
+        this->C = {this->refPoint.x + this->side / 2.0, this->refPoint.y - h};
     }
-
+ 
 public:
     Triangle(const string& n, Point oroinTseg, double s)
-            : Shape2D(n, oroinTseg), side(s) {
-        calculateVertices();
+        : Shape2D(n, oroinTseg), side(s) {
+        this->calculateVertices();
     }
-    double area() const {
-        return sqrt(3.0) / 4.0 * side * side;
+ 
+    double area() const override {
+        return sqrt(3.0) / 4.0 * this->side * this->side;
     }
-
-    double perimeter() const {
-        return 3 * side;
+ 
+    double perimeter() const override {
+        return 3 * this->side;
     }
-
-    void printInfo() const {
+ 
+    void printInfo() const override {
         cout << "===== Zuv Gurvaljin =====\n";
-        cout << "Ner: " << name << "\n";
-        cout << "Taliin urt: " << side << "\n";
+        cout << "Ner: " << this->name << "\n";
+        cout << "Taliin urt: " << this->side << "\n";
         cout << "Oroinuud:\n";
-        cout << "A(" << A.x << ", " << A.y << ")\n";
-        cout << "B(" << B.x << ", " << B.y << ")\n";
-        cout << "C(" << C.x << ", " << C.y << ")\n";
-        cout << "Talbai: " << area() << "\n";
-        cout << "Hureenii urt: " << perimeter() << "\n\n";
+        cout << "A(" << this->A.x << ", " << this->A.y << ")\n";
+        cout << "B(" << this->B.x << ", " << this->B.y << ")\n";
+        cout << "C(" << this->C.x << ", " << this->C.y << ")\n";
+        cout << "Talbai: " << this->area() << "\n";
+        cout << "Hureenii urt: " << this->perimeter() << "\n\n";
     }
 };
-
+ 
 #endif
