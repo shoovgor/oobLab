7b7aee77768c01ed83d7c7d14b84976beb3dd616
Person B <24b1num2408@stud.num.edu.mn>
2026-04-27 22:42:19 +0800

    Circle classig this ashigladag bolgoson

--- lab7/Circle.h
+++ lab7/Circle.h
@@ -1,35 +1,36 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
-
+ 
 #include "PI.h"
 #include "Shape2D.h"
 #include "Point.h"
 #include <iostream>
-
 using namespace std;
+ 
 class Circle : public Shape2D {
 private:
     double radius;
-
+ 
 public:
     Circle(const string& n, Point center, double r)
-            : Shape2D(n, center), radius(r) {}
-
-    double area() const {
-        return PI * radius * radius;
+        : Shape2D(n, center), radius(r) {}
+ 
+    double area() const override {
+        return PI * this->radius * this->radius;
     }
-
-    double perimeter() const {
-           return 2 * PI * radius;
+ 
+    double perimeter() const override {
+        return 2 * PI * this->radius;
     }
-
-    void printInfo() const {
+ 
+    void printInfo() const override {
         cout << "===== Circle =====\n";
-        cout << "Ner: " << name << "\n";
-        cout << "Tuv: (" << refPoint.x << ", " << refPoint.y << ")\n";
-        cout << "Radius: " << radius << "\n";
-           cout << "Talbai: " << area() << "\n";
-        cout << "Hureenii urt: " << perimeter() << "\n\n";
+        cout << "Ner: " << this->name << "\n";
+        cout << "Tuv: (" << this->refPoint.x << ", " << this->refPoint.y << ")\n";
+        cout << "Radius: " << this->radius << "\n";
+        cout << "Talbai: " << this->area() << "\n";
+        cout << "Hureenii urt: " << this->perimeter() << "\n\n";
     }
 };
+ 
 #endif
