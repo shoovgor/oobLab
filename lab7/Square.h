2d8c0e9fd9af51630c764f728b20a03b3c60cec3
Person B <24b1num2408@stud.num.edu.mn>
2026-04-27 22:43:12 +0800

    Square classig this ashigladag bolgoson

--- lab7/Square.h
+++ lab7/Square.h
@@ -1,49 +1,49 @@
 #ifndef SQUARE_H
 #define SQUARE_H
-
+ 
 #include "Shape2D.h"
 #include "Point.h"
 #include <iostream>
 using namespace std;
-
+ 
 class Square : public Shape2D {
-    private:
-        double side;
-        Point A, B, C, D;
-
-        void calculateVertices() {
-            A = refPoint;
-            B = {refPoint.x + side, refPoint.y};
-            C = {refPoint.x + side, refPoint.y - side};
-            D = {refPoint.x, refPoint.y - side};
-        }
-
-    public:
-        Square(const string& n, Point topLeft, double s)
-                : Shape2D(n, topLeft), side(s) {
-            calculateVertices();
-        }
-
-        double area() const {
-            return side * side;
-        }
-
-        double perimeter() const {
-            return 4 * side;
-        }
-
-        void printInfo() const {
-            cout << "===== Square =====\n";
-            cout << "Ner: " << name << "\n";
-            cout << "Taliin urt: " << side << "\n";
-            cout << "Oroinuud:\n";
-            cout << "A(" << A.x << ", " << A.y << ")\n";
-            cout << "B(" << B.x << ", " << B.y << ")\n";
-            cout << "C(" << C.x << ", " << C.y << ")\n";
-            cout << "D(" << D.x << ", " << D.y << ")\n";
-            cout << "Talbai: " << area() << "\n";
-            cout << "Hureenii urt: " << perimeter() << "\n\n";
-        }
-    };
-
+private:
+    double side;
+    Point A, B, C, D;
+ 
+    void calculateVertices() {
+        this->A = this->refPoint;
+        this->B = {this->refPoint.x + this->side, this->refPoint.y};
+        this->C = {this->refPoint.x + this->side, this->refPoint.y - this->side};
+        this->D = {this->refPoint.x, this->refPoint.y - this->side};
+    }
+ 
+public:
+    Square(const string& n, Point topLeft, double s)
+        : Shape2D(n, topLeft), side(s) {
+        this->calculateVertices();
+    }
+ 
+    double area() const override {
+        return this->side * this->side;
+    }
+ 
+    double perimeter() const override {
+        return 4 * this->side;
+    }
+ 
+    void printInfo() const override {
+        cout << "===== Square =====\n";
+        cout << "Ner: " << this->name << "\n";
+        cout << "Taliin urt: " << this->side << "\n";
+        cout << "Oroinuud:\n";
+        cout << "A(" << this->A.x << ", " << this->A.y << ")\n";
+        cout << "B(" << this->B.x << ", " << this->B.y << ")\n";
+        cout << "C(" << this->C.x << ", " << this->C.y << ")\n";
+        cout << "D(" << this->D.x << ", " << this->D.y << ")\n";
+        cout << "Talbai: " << this->area() << "\n";
+        cout << "Hureenii urt: " << this->perimeter() << "\n\n";
+    }
+};
+ 
 #endif
